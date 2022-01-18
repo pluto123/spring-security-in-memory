eb090cf3f6e9f41e9c3e887e371466bdba0e3d6b
Steven Wu <stevenwu@mirle.com.tw>
2022-01-18 11:27:50 +0800

    to initialize project

---
+++ src/test/java/tw/com/pluto/SpringSecurityInMemoryApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.com.pluto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityInMemoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
