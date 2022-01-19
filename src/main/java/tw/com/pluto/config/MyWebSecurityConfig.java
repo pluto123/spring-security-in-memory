16a49511e6f15c84a1725417ca44751212662633
Steven Wu <stevenwu@mirle.com.tw>
2022-01-19 10:49:27 +0800

    將密碼加密

--- src/main/java/tw/com/pluto/config/MyWebSecurityConfig.java
+++ src/main/java/tw/com/pluto/config/MyWebSecurityConfig.java
@@ -4,15 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import tw.com.pluto.service.MyUserDetailsService;
 
 @Configuration
 public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
-    UserDetailsService myUserDetailsService;
+    MyUserDetailsService myUserDetailsService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(myUserDetailsService);
+        auth.userDetailsService(myUserDetailsService).passwordEncoder(myUserDetailsService.passwordEncoder());
     }
 }
--- src/main/java/tw/com/pluto/service/MyUserDetailsService.java
+++ src/main/java/tw/com/pluto/service/MyUserDetailsService.java
@@ -1,18 +1,27 @@
 package tw.com.pluto.service;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyUserDetailsService implements UserDetailsService {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserDetails userDetails = User.builder()
                 .username("user123")
-                .password("{noop}user123") // 密碼前面加上"{noop}"，不對密碼進行任何格式的編碼。
+                .password(passwordEncoder().encode("user123")) // 對密碼加密，不儲存明碼
                 .roles("USER")
                 .build();
 
